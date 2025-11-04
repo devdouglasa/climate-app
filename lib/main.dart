6a20a4a38299b44631dac04ac8afd7a1fbcd55d9
devdouglasa <douglasalisson27@gmail.com>
2025-11-04 10:26:20 -0300

    refactor: unnecessary import corrected

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'package:climate_app/screens/home_screen.dart';
 import 'package:climate_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
